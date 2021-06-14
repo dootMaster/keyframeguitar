df3627239bf391ac4d2e23c944cf8366b5c840c5
Leslie Ngo <ngo.leslie@gmail.com>
2021-06-14 16:24:37 -0700

    changed 'dist' to 'docs' for github pages

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../docs')));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
